8701d73f757e8bacb6e69234d9f5b838c8a67087
Benjamin Skinner <btskinner@coe.ufl.edu>
2020-01-07 09:40:36 -0500

    updates for init class

---
+++ lessons/knit_setup.R
@@ -0,0 +1,29 @@
+## general knit options
+knitr::opts_chunk$set(base.url="../",
+                      fig.path="figures/",
+                      dpi=90,
+                      out.width="100%",
+                      error = TRUE,
+                      comment = NA)
+
+## limit output lines (h/t https://stackoverflow.com/a/23147563)
+hook_output <- knitr::knit_hooks$get("output")
+knitr::knit_hooks$set(output = function(x, options) {
+  lines <- options$output.lines
+  if (is.null(lines)) {
+    return(hook_output(x, options))  # pass to default hook
+  }
+  x <- unlist(strsplit(x, "\n"))
+  more <- "..."
+  if (length(lines)==1) {        # first n lines
+    if (length(x) > lines) {
+      # truncate the output, but add ....
+      x <- c(head(x, lines), more)
+    }
+  } else {
+    x <- c(more, x[lines], more)
+  }
+  # paste these lines together
+  x <- paste(c(x, ""), collapse = "\n")
+  hook_output(x, options)
+})
---
+++ scripts/intro.R
@@ -0,0 +1,55 @@
+################################################################################
+##
+## <PROJ> EDH7916: Introduction
+## <FILE> introduction.R 
+## <INIT> 6 January 2020
+## <AUTH> Benjamin Skinner (GitHub/Twitter: @btskinner)
+##
+################################################################################
+
+
+## ---------------------------------------------------------
+## Language and environment
+## ---------------------------------------------------------
+
+## run me first
+print('Hello, World!')
+
+
+## ---------------------------------------------------------
+## Assignment
+## ---------------------------------------------------------
+
+
+## assign value to object x using <-
+x <- 1
+
+
+## when you input a number or character, R returns it back to you
+1
+"a"
+
+
+## what's in x?
+x
+
+
+## wrap in () to print after assignment
+(x <- 5)
+
+
+
+
+
+## ---------------------------------------------------------
+## Help
+## ---------------------------------------------------------
+
+
+## get help file for function
+?median
+
+
+## search for function in CRAN
+??median
+
