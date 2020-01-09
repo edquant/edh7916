67112f4afdd3f7e2c6cc01d608a7143d82208baa
btskinner <btskinner@coe.ufl.edu>
2020-01-09 18:26:05 -0500

    v1.1.2 release

---
+++ assets/js/git_labels.js
@@ -0,0 +1,23 @@
+var urlToGetLabels = "https://api.github.com/repos/edquant/edh7916/labels";
+
+function returnColor(val) {
+    
+}
+
+$("#gitlabels").append("<ul id='gl_newlist'></ul>");
+$(document).ready(function () {
+    $.getJSON(urlToGetLabels, function (allLabels) {
+        $.each(allLabels, function (i, label) {
+	    var textcolor = (label.name == "Bug" || label.name == "Suggestion") ? "white" : "black";
+            $("#gl_newlist")
+	       .append("<li><span class=\"IssueLabel--big d-inline-block v-align-top lh-condensed js-label-link gitlabel\"" +
+		       " style=\"background-color: #" +
+		       label.color +
+		       "; color: " +
+		       textcolor +
+		       ";\">" +
+		       label.name +
+		       "</span></li>");
+        });
+    });
+});
