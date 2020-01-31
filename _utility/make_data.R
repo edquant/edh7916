58786a112152ea74e256e8dae036f3cadc1cf184
btskinner <btskinner@coe.ufl.edu>
2020-01-31 15:42:34 -0500

    Added small data files to use for data appending, merging, and reshaping.

---
+++ _utility/make_data.R
@@ -0,0 +1,12 @@
+################################################################################
+##
+## [ PROJ ] EDH7916: Contemporary Research in Higher Education
+## [ FILE ] make_data.r
+## [ AUTH ] Benjamin Skinner
+## [ INIT ] 17 January 2020
+##
+################################################################################
+
+## libraries
+library(tidyverse)
+
